68b62924b4310b5897ef851c1c01d2a3b75cb67a
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-25 23:21:01 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,17 @@ class Book
   
   def initialize(title)
   @title = title
-end 
+  end
+  
+  def title
+  end
+  
+  def author 
+  end 
+  
+  def page_count
+  end 
+  
 end 
 
 Book.new("And Then There Were None")
